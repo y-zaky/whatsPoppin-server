35e93d97fffdddc28825653c48e526deeae9f326
Yahia Zaky <zaky20@hotmail.com>
2018-07-14 19:07:55 +0100

    recieve response from server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.use(
+  function (req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    // Set custom headers for CORS
+    res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    res.status(200)
+    return next();
+  }
+)
+
+app.get( '/', (req,res) => {
+  console.log(req)
+  
+  res.send(JSON.stringify("Hello World"))
+})
+
+app.listen(1234, () => console.log('App live and listening on port 1234!'))
