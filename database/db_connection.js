3602bb094b277b6aac45670dca272a80d3a37fca
Yahia Zaky <zaky20@hotmail.com>
2018-07-15 20:12:10 +0100

    db connection.js

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,6 +1,23 @@
 //db
 const { Client } = require('pg')
-const client = new Client()
+const url = require('url');
+
+if (!process.env.DB_URL) throw new Error('Enviroment variable DB_URL must be set');
+
+const params = url.parse(process.env.DB_URL);
+const [username, password] = params.auth.split(':');
+
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  ssl: params.hostname !== 'localhost'
+};
+
+module.exports = new Client(options);
 
 // await client.connect()
 
@@ -8,11 +25,12 @@ const client = new Client()
 // console.log(res.rows[0].message) // Hello world!
 // await client.end()
 
-client.connect()
+// client.connect()
 
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  console.log('ress',res)
-  client.end()
-})
+// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+//   console.log(err ? err.stack : res.rows[0].message) // Hello World!
+//   console.log('ress',res)
+//   client.end()
+// })
+
