7f8bf9d8215a06810d2d5d107d19d30c0fb86bae
Yahia Zaky <zaky20@hotmail.com>
2018-07-15 22:05:17 +0100

    .env to gitignore and env install

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,6 +1,7 @@
 //db
 const { Client } = require('pg')
 const url = require('url');
+require('env2')('./config.env');
 
 if (!process.env.DB_URL) throw new Error('Enviroment variable DB_URL must be set');
 
